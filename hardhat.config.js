65f45eaa479bd031d84221f5b3909fdd0b5ee4b2
Matt Rogers <rogers.matthew35@gmail.com>
2024-07-25 16:57:44 -0500

    set up config

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+//REMEMBER: also add other nomic tools if neccessary.
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    mumbai: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
+};
